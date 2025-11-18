d77dee639f1de119ea9351b3387430f33e528793
Dan Moffat <danmoffat@gmail.com>
2025-11-18 23:05:06 +0000

    Variable jump strength on key down hold

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import kaplay from 'kaplay';
 import './App.css';
 
 function App() {
   const game = useRef(null);
+  const [keyDownStart, setKeyDownStart] = useState(10);
 
   useEffect(() => {
     const k = kaplay({
@@ -16,21 +17,26 @@ function App() {
       canvas: game.current
     });
 
-    const obj = k.add([k.rect(32, 32), k.pos(10, 20), k.color(255, 255, 255), k.area(), k.body(), 'shape']);
+    k.scene('game', () => {
+      const obj = k.add([k.rect(32, 32), k.pos(10, 20), k.color(255, 255, 255), k.area(), k.body(), 'player']);
 
-    k.add([k.rect(500, 2), k.pos(0, 190), k.color(255, 255, 255), k.area(), k.body({ isStatic: true }), 'shape']);
+      k.add([k.rect(500, 2), k.pos(0, 190), k.color(255, 255, 255), k.area(), k.body({ isStatic: true }), 'player']);
 
-    k.setGravity(200);
+      k.setGravity(1000);
 
-    k.onKeyDown('right', () => {
-      obj.move(200, 0);
+      let keyDown;
+
+      k.onKeyPress('space', () => {
+        keyDown = Date.now();
+      });
+
+      k.onKeyRelease('space', () => {
+        const jumpStrength = Date.now() - keyDown;
+        obj.jump(jumpStrength > 500 ? 500 : jumpStrength);
+      });
     });
-    k.onKeyDown('left', () => {
-      obj.move(-200, 0);
-    });
-    k.onKeyPress('space', () => {
-      obj.jump(100);
-    });
+
+    k.go('game');
   }, []);
 
   return <canvas ref={game} id='game'></canvas>;
