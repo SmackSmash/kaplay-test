7ce8313f84d01d43a8c7db8eb099eff8813025aa
Dan Moffat <danmoffat@gmail.com>
2025-11-18 23:21:31 +0000

    Updated collision

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,32 @@ function App() {
     });
 
     k.scene('game', () => {
-      const obj = k.add([k.rect(32, 32), k.pos(10, 20), k.color(255, 255, 255), k.area(), k.body(), 'player']);
+      const obj = k.add([k.rect(32, 32), k.pos(30, 20), k.color(255, 255, 255), k.area(), k.body(), 'player']);
 
-      k.add([k.rect(500, 2), k.pos(0, 190), k.color(255, 255, 255), k.area(), k.body({ isStatic: true }), 'player']);
+      k.add([k.rect(500, 2), k.pos(0, 198), k.color(0, 255, 0), k.area(), k.body({ isStatic: true }), 'ground']);
 
       k.setGravity(1000);
+
+      const obstacle = k.add([
+        k.rect(25, 25),
+        k.pos(500, 198),
+        k.color(255, 0, 0),
+        k.area(),
+        k.body({ isStatic: true }),
+        { time: 0 },
+        'obstacle'
+      ]);
+
+      obstacle.onUpdate(() => {
+        obstacle.time += k.dt();
+        const t = (obstacle.time % 5) / 2;
+        obstacle.pos = k.lerp(k.vec2(500, 173), k.vec2(0, 173), t);
+      });
+
+      k.onCollide('player', 'obstacle', () => {
+        alert('You Lose!');
+        k.quit();
+      });
 
       let keyDown;
 
